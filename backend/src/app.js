f9b52500e4552046d5a5502a7e4c0456c2ddcec8
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-07-02 13:07:39 +0530

    Explored Http requests.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.send("api is hitting"));
 
+app.post('/user', (req, res) => res.send("Data save successfully."));
+app.delete('/user', (req, res) => res.send("Data deleted successfully."));
+app.patch('/user', (req, res) => res.send("Data patched successfully."));
+
 app.get('/test', (req, res) => res.send("I am testing"));
-app.get('/user', (req, res) => res.send("I am user"));
+app.get('/user', (req, res) => res.send({"FirstName" : "Shraddha", "LastName" : "Gaur"}));
 
 app.listen(3000, () => console.log("server running on port 3000"));
